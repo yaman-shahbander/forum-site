154f31718879be73011ed39f02508b2160706b3d
yaman-shahbandar <yaman.job.work@gmail.com>
2022-10-13 17:46:50 +0300

    feature/add-channel-filter: finish adding tests for filtering threads according to channel with applying that functionality to front-end

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Channel extends Model
 {
     use HasFactory;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function threads(): HasMany
+    {
+        return $this->hasMany(Thread::class);
+    }
 }
